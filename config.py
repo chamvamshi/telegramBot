59dcd3a277f0e52812e187b979c0c78b897ff4c5
chamvamshi <kasaramvamshi7143@gmail.com>
2025-10-08 12:37:18 +0530

    updated config.py code

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-OPENROUTER_API_KEY = "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"  # Your OpenRouter key
+OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
